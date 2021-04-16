fd0f0a815a5287c5beca3a37299486c0649d6ecb
DwiZaki <dwi.zaki.123@gmail.com>
2021-04-16 10:23:51 +0700

    Tugas 7

---
+++ I0320029_soal 1_Tugas7.py
@@ -0,0 +1,20 @@
+string = "garuda wisnu kencana"
+
+# Capitalize
+cap = string.capitalize()
+print("string setelah di capitalize: \n", cap)
+print()
+
+# Center
+cen = string.center(50,'-')
+print("string setelah di center: \n", cen)
+print()
+
+# Count
+x = string.count('a')
+print('Jumlah huruf a =', x)
+print()
+
+# upper 
+up = string.upper()
+print('string setelah di upper; \n', up)
---
+++ I0320029_soal 2_Tugas7.py
@@ -0,0 +1,26 @@
+x = 86.02
+y = -21.52
+
+# ceil
+import math
+print('x = ', x)
+print('y = ', y)
+print()
+print('nilai x ceil = ', math.ceil(x))
+print('nilai y ceil = ', math.ceil(y))
+print()
+
+# fabs
+print('nilai math.fabs x = ', math.fabs(x))
+print('nilai math.fabs y = ', math.fabs(y))
+print()
+
+# floor
+print('nilai math.floor x = ', math.floor(x))
+print('nilai math.floor y = ', math.floor(y))
+print()
+
+# sqrt
+print('nilai akar kuadrat dari x = {:.2f}'.format(math.sqrt(x)))
+print('nilai akar kuadrat dari y = {:.2f}'.format(math.sqrt(math.fabs(y))))
+print()
